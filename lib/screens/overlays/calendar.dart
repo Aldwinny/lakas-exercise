e00d53b4d9be10af45c1fa11b459acf512857125
Aldwinny <aldwin0313@yahoo.com.ph>
2023-10-29 16:55:31 +0800

    Updated calendar.dart

--- lib/screens/overlays/calendar.dart
+++ lib/screens/overlays/calendar.dart
@@ -12,6 +12,47 @@ class CalendarScreen extends StatefulWidget {
 class _CalendarScreenState extends State<CalendarScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      child: const Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Hello',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Hello', style: TextStyle(fontWeight: FontWeight.bold))
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Hello', style: TextStyle(fontWeight: FontWeight.bold))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CalendarNumberBuilder extends StatelessWidget {
+  const CalendarNumberBuilder(this.number, {super.key});
+
+  final List<String> number;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(children: number.map<Widget>((e) => Text(e)).toList()),
+    );
   }
 }
